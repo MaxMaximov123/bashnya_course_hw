7e10b8457b0842251c45896da391b85a9aa3f1f7
Макс Максимов <maxss.kzn@gmail.com>
2025-10-20 10:04:48 +0300

    create task1 easy version

--- bashnya-hw2/main.go
+++ bashnya-hw2/main.go
@@ -5,5 +5,42 @@ import (
 )
 
 func main() {
-	fmt.Println("test")
+	var n int
+
+	fmt.Print("Введите число: ")
+	fmt.Scan(&n)
+
+	new_n := n
+
+	if n >= 12307 {
+		fmt.Println("Введенное число больше или равно 12307")
+		return
+	}
+
+	for n < 12307 {
+		if n%9 == 0 && n%13 == 0 {
+			fmt.Println(n)
+			fmt.Println("service error")
+			break
+		} else {
+			n++
+		}
+
+		if n < 0 {
+			n *= -1
+		} else if n%7 == 0 {
+			n *= 39
+		} else if n%9 == 0 {
+			n = (n * 13) + 1
+			continue
+		} else {
+			n = (n + 2) * 3
+		}
+
+	}
+
+	if !(n%13 == 0 && n%9 == 0) {
+		fmt.Printf("В ходе выполнения программы число %v было преобразовано в число %v", new_n, n)
+	}
+
 }
